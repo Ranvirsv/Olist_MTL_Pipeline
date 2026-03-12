04f445d4d1918b1a97b2a73a024ce59ee15dbb98
ranivrsv <ranvirsv@gmail.com>
2026-03-12 18:26:00 +0530

    Update: sql

--- sql/build_db.sql
+++ sql/build_db.sql
@@ -1,12 +1,12 @@
 -- 1. Ingest All 8 Raw CSVs into DuckDB Tables
-CREATE OR REPLACE TABLE customers AS SELECT * FROM read_csv_auto('/Users/ranvirsinghvirk/Eclipse/DS Practice/Olist_MTL_Pipeline/data/raw/olist_customers_dataset.csv');
-CREATE OR REPLACE TABLE orders AS SELECT * FROM read_csv_auto('/Users/ranvirsinghvirk/Eclipse/DS Practice/Olist_MTL_Pipeline/data/raw/olist_orders_dataset.csv');
-CREATE OR REPLACE TABLE reviews AS SELECT * FROM read_csv_auto('/Users/ranvirsinghvirk/Eclipse/DS Practice/Olist_MTL_Pipeline/data/raw/olist_order_reviews_dataset.csv');
-CREATE OR REPLACE TABLE items AS SELECT * FROM read_csv_auto('/Users/ranvirsinghvirk/Eclipse/DS Practice/Olist_MTL_Pipeline/data/raw/olist_order_items_dataset.csv');
-CREATE OR REPLACE TABLE products AS SELECT * FROM read_csv_auto('/Users/ranvirsinghvirk/Eclipse/DS Practice/Olist_MTL_Pipeline/data/raw/olist_products_dataset.csv');
-CREATE OR REPLACE TABLE sellers AS SELECT * FROM read_csv_auto('/Users/ranvirsinghvirk/Eclipse/DS Practice/Olist_MTL_Pipeline/data/raw/olist_sellers_dataset.csv');
-CREATE OR REPLACE TABLE payments AS SELECT * FROM read_csv_auto('/Users/ranvirsinghvirk/Eclipse/DS Practice/Olist_MTL_Pipeline/data/raw/olist_order_payments_dataset.csv');
-CREATE OR REPLACE TABLE geolocation AS SELECT * FROM read_csv_auto('/Users/ranvirsinghvirk/Eclipse/DS Practice/Olist_MTL_Pipeline/data/raw/olist_geolocation_dataset.csv');
+CREATE OR REPLACE TABLE customers AS SELECT * FROM read_csv_auto('/data/raw/olist_customers_dataset.csv');
+CREATE OR REPLACE TABLE orders AS SELECT * FROM read_csv_auto('/data/raw/olist_orders_dataset.csv');
+CREATE OR REPLACE TABLE reviews AS SELECT * FROM read_csv_auto('/data/raw/olist_order_reviews_dataset.csv');
+CREATE OR REPLACE TABLE items AS SELECT * FROM read_csv_auto('/data/raw/olist_order_items_dataset.csv');
+CREATE OR REPLACE TABLE products AS SELECT * FROM read_csv_auto('/data/raw/olist_products_dataset.csv');
+CREATE OR REPLACE TABLE sellers AS SELECT * FROM read_csv_auto('/data/raw/olist_sellers_dataset.csv');
+CREATE OR REPLACE TABLE payments AS SELECT * FROM read_csv_auto('/data/raw/olist_order_payments_dataset.csv');
+CREATE OR REPLACE TABLE geolocation AS SELECT * FROM read_csv_auto('/data/raw/olist_geolocation_dataset.csv');
 
 -- 2. Build the Unified Analytical View
 CREATE OR REPLACE VIEW analytical_base_table AS 
